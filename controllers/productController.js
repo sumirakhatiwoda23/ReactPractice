46728087141304cd6902d2fb0d58a50c46e9bf90
sumira2222 <sumirakhatiwoda23@gmail.com>
2026-05-15 12:53:03 +0545

    backedn upto date

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 import Product from "../models/Product.js";
-
+import fs from 'fs';
 
 
 export const getProducts = async (req, res) => {
@@ -17,16 +17,26 @@ export const getProducts = async (req, res) => {
 }
 
 
-export const getProduct = (req, res) => {
-  return res.status(200).json({
-    message: "Single product"
-  });
+export const getProduct = async (req, res) => {
+
+
+  try {
+    const product = await Product.findById(req.productId);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    return res.status(200).json(product);
+  } catch (err) {
+
+    return res.status(400).json({
+      message: err.message
+    })
+  }
+
 }
 
 
 export const createProduct = async (req, res) => {
 
-  const { title, description, price, category, brand, stock } = req.body;
+  const { title, description, price, category, brand, stock } = req.body || {};
 
   try {
 
@@ -45,21 +55,103 @@ export const createProduct = async (req, res) => {
 
 
   } catch (err) {
-    return res.status(400).json({
-      message: err.message
+
+    fs.unlink(`./uploads/${req.imagePath}`, (imageErr) => {
+      if (imageErr) {
+        return res.status(500).json({
+          message: imageErr.message
+        })
+      }
+
+      return res.status(400).json({
+        message: err.message
+      })
     })
+
   }
 
 }
 
-export const updateProduct = (req, res) => {
-  return res.status(200).json({
-    message: "Product updated"
-  });
+export const updateProduct = async (req, res) => {
+  const { title, description, price, category, brand, stock } = req.body || {};
+
+  try {
+
+    const isExist = await Product.findById(req.productId);
+    if (!isExist) return res.status(404).json({ message: "Product not found" });
+
+    isExist.title = title || isExist.title;
+    isExist.description = description || isExist.description;
+    isExist.price = price || isExist.price;
+    isExist.category = category || isExist.category;
+    isExist.brand = brand || isExist.brand;
+    isExist.stock = stock || isExist.stock;
+
+
+    if (req.imagePath) {
+
+      fs.unlink(`./uploads/${isExist.image}`, async (err) => {
+        if (err) return res.status(500).json({
+          message: err.message
+        })
+        await isExist.save();
+        return res.status(200).json({
+          message: "Product updated"
+        });
+
+      });
+
+    } else {
+
+      await isExist.save();
+      return res.status(200).json({
+        message: "Product updated"
+      });
+
+    }
+
+
+
+  } catch (err) {
+    if (req.imagePath) {
+      fs.unlink(`./uploads/${req.imagePath}`, (imageErr) => {
+        if (imageErr) {
+          return res.status(500).json({
+            message: imageErr.message
+          })
+        }
+
+        return res.status(400).json({
+          message: err.message
+        })
+      })
+    } else {
+      return res.status(400).json({
+        message: err.message
+      })
+    }
+  }
 }
 
-export const deleteProduct = (req, res) => {
-  return res.status(200).json({
-    message: "Product deleted"
-  });
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.productId);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    fs.unlink(`./uploads/${product.image}`, (err) => {
+      if (err) {
+        return res.status(500).json({
+          message: err.message
+        })
+      }
+      product.deleteOne();
+      return res.status(200).json(product);
+    })
+
+
+  } catch (err) {
+
+    return res.status(400).json({
+      message: err.message
+    })
+  }
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,14 +2,21 @@ import express from 'express';
 import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/productController.js';
 import { notAllowed } from '../utils/notAllowed.js';
 import { fileCheck } from '../middleware/fileCheck.js';
-
+import mongoose from 'mongoose';
 
 
 
 const router = express.Router();
 
 
-router.route('/').get(getProducts).post(fileCheck,createProduct).all(notAllowed);
+router.param('id', (req, res, next, id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: "Invalid product id" });
+  req.productId = id;
+  next();
+});
+
+router.route('/').get(getProducts).post(fileCheck, createProduct).all(notAllowed);
+
 router.route('/:id').get(getProduct).patch(updateProduct).delete(deleteProduct);
 
 
